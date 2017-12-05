fc0e3a19c8db616efaf809209b567c679e2b7deb
Cody Crewson <6089951+crcrewso@users.noreply.gitbub.com>
2017-12-04 20:47:57 -0600

    Question 13 Solution

--- Python/Q0013.py
+++ Python/Q0013.py
@@ -1,4 +1,22 @@
 """Project Euler Question 13
 
+Work out the first ten digits of the sum of the following one-hundred 50-digit numbers.
+Saved in Q0013.txt
 
 """
+
+
+numbers = []
+f = open("Q0013.txt", 'r')
+for line in f:
+    numbers.append(int(line))
+
+sum = 0.0
+
+for num in numbers:
+    sum += num
+
+while sum > 10000000000:
+    sum /= 10
+
+print(str(int(sum)))
