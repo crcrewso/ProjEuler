6393a1b3b10e15bfcaa9d377c35e6c546bb05cae
Icaarus <crcrewso@me.com>
2018-01-27 00:15:40 -0600

    Question 25 Solutions

--- Python/Q0025.py
+++ Python/Q0025.py
@@ -1,4 +1,4 @@
-""" Project Euler Question 24
+""" Project Euler Question 25
 
 The Fibonacci sequence is defined by the recurrence relation:
 
@@ -21,15 +21,28 @@ The 12th term, F12, is the first term to contain three digits.
 
 What is the index of the first term in the Fibonacci sequence to contain 1000 digits?
 
+Used a lot of math tricks
+
 """
 
 import math
 
-fiblist = [1,1]
+fibold = 1.
+fibnew = 1.
 
+fibindex = 2  # index of fibnew
 
-while(True):
-    fiblist.append(fiblist[-1] + fiblist[-2])
-    if fiblist[-1] > 1e999:
-        print(fiblist[-1])
-        exit()
+print()
+
+digits = 1
+
+while (digits < 1000):
+    temp = fibnew + fibold
+    fibold = fibnew
+    fibnew = temp
+    fibindex += 1
+    if fibnew >= 10:  # keep removing last digit every time it goes above 10 so that the number doesn't get bigger than we need
+        fibold /= 10
+        fibnew /= 10
+        digits += 1
+print(fibindex)
